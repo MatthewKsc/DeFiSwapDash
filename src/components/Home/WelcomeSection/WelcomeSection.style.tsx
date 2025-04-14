9c6888322dd517a864e9fa30df2f1380a9cae47b
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2025-04-14 22:06:14 +0200

    Move most of inline styles into styled components

--- src/components/Home/WelcomeSection/WelcomeSection.style.tsx
+++ src/components/Home/WelcomeSection/WelcomeSection.style.tsx
@@ -1,7 +1,24 @@
-import { styled, Box } from "@mui/material";
+import { styled, Box, Grid } from "@mui/material";
 
 export const WelcomeSectionBox = styled(Box)(({ theme }) => ({
     width: '100%',
     padding: '5rem 0',
     backgroundColor: theme.palette.background.default,
+}));
+
+export const WelcomeImageBox = styled('img')(() => ({
+    width: '100%',
+    height: 'auto',
+    borderRadius: '8px',
+}));
+
+export const WelcomeGridContainer = styled(Grid)(({ theme }) => ({
+    width: '100%',
+    maxWidth: theme.breakpoints.values.xl,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    'h3': {
+        color: theme.palette.text.secondary,
+        marginBottom: '32px',
+    }
 }));
